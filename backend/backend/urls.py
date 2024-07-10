ee24fb0698761c3b2c7c266cb404f136c29c5e01
Colton - Work <colthyland@gmail.com>
2024-07-10 12:30:01 -0700

    urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, QuestionViewSet, AttemptViewSet, QuestionHistoryViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'questions', QuestionViewSet)
+router.register(r'attempts', AttemptViewSet)
+router.register(r'history', QuestionHistoryViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
