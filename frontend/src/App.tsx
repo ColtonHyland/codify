29d36b7ccce6554fab7c70a8a35cb4b5f0569291
Colton - Work <colthyland@gmail.com>
2024-07-17 15:02:13 -0700

    pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignUpPage from './pages/SignUpPage';
+import SignInPage from './pages/SignInPage';
+import SignOutPage from './pages/SignOutPage';
+import './assets/styles/App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signout" element={<SignOutPage />} />
+        <Route path="/" element={<SignInPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/pages/SignOutPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SignOut from '../components/auth/SignOut';
+
+const SignOutPage: React.FC = () => {
+  return <SignOut />;
+};
+
+export default SignOutPage;
