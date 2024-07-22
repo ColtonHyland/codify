cbf6ebd585dba855c7c26336e89641a02a6e72fd
Colton - Work <colthyland@gmail.com>
2024-07-21 22:28:09 -0700

    react json tree for easy viewing

--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -1,1 +1,11 @@
-/// <reference types="react-scripts" />
+declare module 'react-json-tree' {
+  import * as React from 'react';
+
+  interface JSONTreeProps {
+    data: any;
+    theme?: any;
+    invertTheme?: boolean;
+  }
+
+  export default class JSONTree extends React.Component<JSONTreeProps, any> {}
+}
