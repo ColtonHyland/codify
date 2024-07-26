9db3fec3c3137d1763cef46ae3e7f80406c27ce5
Colton - Work <colthyland@gmail.com>
2024-07-26 14:48:16 -0700

    test returns code- endpoint succesfully accepts front end input

--- frontend/src/services/codeExecute.tsx
+++ frontend/src/services/codeExecute.tsx
@@ -3,10 +3,13 @@ import { ExecuteCode } from "../types";
 
 export const executeCode = async (params: ExecuteCode): Promise<any> => {
   try {
-    const response = await axios.post('http://localhost:8000/api/code/execute', {
+    const token = localStorage.getItem("token");
+    const response = await axios.post('http://localhost:8000/api/code/execute/', {
       code: params.code,
       language: params.language,
       test_cases: params.test_cases
+    }, {
+      headers: { Authorization: `Token ${token}` }
     });
     return response.data;
   } catch (error) {
