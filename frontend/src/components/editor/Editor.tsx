54adfb330068e1de0ecf1168ff824bd94d545929
Colton - Work <colthyland@gmail.com>
2024-07-26 13:09:24 -0700

    editor component using monaco

---
+++ frontend/src/components/editor/Editor.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import MonacoEditor from 'react-monaco-editor';
+import { EditorProps } from '../../types';
+
+const Editor: React.FC<EditorProps> = ({ language, code, setCode }) => {
+  return (
+    <MonacoEditor
+      width="100%"
+      height="600px"
+      language={language}
+      theme="vs-dark"
+      value={code}
+      onChange={(newValue) => setCode(newValue)}
+    />
+  );
+};
+
+export default Editor;
