6dc73ae875786d1a9812d34ee800cc85cd620332
Colton - Work <colthyland@gmail.com>
2024-08-31 18:10:29 -0700

    navbar work

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -3,6 +3,9 @@ import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
 import whiteFavicon from '../../assets/images/favicon_white_cleaned.png';
+import LogoutIcon from '@mui/icons-material/Logout';
+import LoginIcon from '@mui/icons-material/Login';
+
 
 const Navbar: React.FC = () => {
   const { user, logout } = useAuth();
@@ -30,27 +33,39 @@ const Navbar: React.FC = () => {
           <img src={whiteFavicon} alt="Logo" style={{height: '48px' }} />
           odify
         </Button>
+        {user && (
+        <Button color="inherit" component={Link} to="/questions"
+        sx={{
+          flexGrow: 1,
+          fontSize: "1rem",
+          padding: "12px 24px",
+        }}>
+            Practice
+        </Button>
+        )}
 
         <Box
           sx={{ display: "flex", justifyContent: "flex-end", width: "100%", fontFamily: "'-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'" }}
         >
           {user && (
-            <Typography variant="h6" component="div" sx={{ marginRight: 2 }}>
-              {user.username}
+            <Typography color="inherit" variant="subtitle1" sx={{ marginRight: 2 }}>
+              Ready to code, {user.username}?
             </Typography>
           )}
-          <Button color="inherit" component={Link} to="/questions">
-            Practice
-          </Button>
+          
           {user ? (
             <>
               <Button color="inherit" onClick={handleLogout}>
+              <LogoutIcon sx={{ marginLeft: 1 }}/>
                 Logout
+                
               </Button>
             </>
           ) : (
             <Button color="inherit" component={Link} to="/login">
+              <LoginIcon sx={{ marginLeft: 1 }}/>
               Login
+              
             </Button>
           )}
         </Box>
