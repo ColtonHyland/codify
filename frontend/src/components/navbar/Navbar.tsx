42b386d2da7443162f1261b93e19da7c3c6a339d
Colton - Work <colthyland@gmail.com>
2024-09-01 21:56:40 -0700

    language selector styling

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,15 +1,28 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Box,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Typography,
+} from "@mui/material";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
+import { useLanguage } from "../../contexts/LanguageContext";
 import whiteFavicon from "../../assets/images/favicon_white_cleaned.png";
 import LogoutIcon from "@mui/icons-material/Logout";
 import LoginIcon from "@mui/icons-material/Login";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
-import ImportContactsOutlinedIcon from '@mui/icons-material/ImportContactsOutlined';
+import ImportContactsOutlinedIcon from "@mui/icons-material/ImportContactsOutlined";
+import { FaJs, FaPython, FaDatabase, FaCplusplus } from "react-icons/fa";
 
 const Navbar: React.FC = () => {
   const { user, logout } = useAuth();
+  const { selectedLanguage, setSelectedLanguage } = useLanguage();
 
   const handleLogout = async () => {
     await logout();
@@ -43,23 +56,92 @@ const Navbar: React.FC = () => {
           </Button>
 
           {user && (
-            <Button
-              color="inherit"
-              component={Link}
-              to="/questions"
+            <Box
               sx={{
-                fontSize: "1rem",
-                padding: "6px 12px",
-                border: "2px solid white",
-                borderRadius: "12px",     
-                '&:hover': {
-                  backgroundColor: "rgba(255, 255, 255, 0.1)",
-                }
+                display: "flex",
+                alignItems: "center",
               }}
             >
-              <ImportContactsOutlinedIcon sx={{ marginRight: 1 }} />
-              Practice
-            </Button>
+              <FormControl
+                variant="outlined"
+                sx={{
+                  minWidth: 120,
+                  marginRight: 2,
+                  "& .MuiOutlinedInput-root": {
+                    color: "white",
+                    "& fieldset": {
+                      borderColor: "white",
+                    },
+                    "&:hover fieldset": {
+                      borderColor: "white",
+                    },
+                    "&.Mui-focused fieldset": {
+                      borderColor: "white",
+                    },
+                  },
+                  "& .MuiSvgIcon-root": {
+                    color: "white",
+                  },
+                }}
+              >
+                <Select
+                  id="language-select"
+                  value={selectedLanguage}
+                  onChange={(e) => setSelectedLanguage(e.target.value)}
+                  displayEmpty
+                  inputProps={{ "aria-label": "Without label" }}
+                  sx={{
+                    color: "white",
+                  }}
+                >
+                  <MenuItem
+                    value="JavaScript"
+                    sx={{ backgroundColor: "green", color: "white" }}
+                  >
+                    <FaJs style={{ marginRight: 8 }} />
+                    JavaScript
+                  </MenuItem>
+                  <MenuItem
+                    value="Python"
+                    sx={{ backgroundColor: "green", color: "white" }}
+                  >
+                    <FaPython style={{ marginRight: 8 }} />
+                    Python
+                  </MenuItem>
+                  <MenuItem
+                    value="MySQL"
+                    sx={{ backgroundColor: "green", color: "white" }}
+                  >
+                    <FaDatabase style={{ marginRight: 8 }} />
+                    MySQL
+                  </MenuItem>
+                  <MenuItem
+                    value="C++"
+                    sx={{ backgroundColor: "green", color: "white" }}
+                  >
+                    <FaCplusplus style={{ marginRight: 8 }} />
+                    C++
+                  </MenuItem>
+                </Select>
+              </FormControl>
+              <Button
+                color="inherit"
+                component={Link}
+                to="/questions"
+                sx={{
+                  fontSize: "1rem",
+                  padding: "6px 12px",
+                  border: "2px solid white",
+                  borderRadius: "12px",
+                  "&:hover": {
+                    backgroundColor: "rgba(255, 255, 255, 0.1)",
+                  },
+                }}
+              >
+                <ImportContactsOutlinedIcon sx={{ marginRight: 1 }} />
+                Practice
+              </Button>
+            </Box>
           )}
 
           <Box
