52fc8d7f5c8e7d0b4ea6a21b4834a5bf2ccec68f
Colton - Work <colthyland@gmail.com>
2024-09-02 14:58:56 -0700

    auth button

---
+++ frontend/src/components/navbar/AuthButton.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Button, Typography, Box } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
+import LogoutIcon from "@mui/icons-material/Logout";
+import LoginIcon from "@mui/icons-material/Login";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+
+const AuthButtons: React.FC = () => {
+  const { user, logout } = useAuth();
+
+  const handleLogout = async () => {
+    await logout();
+  };
+
+  return (
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      {user && (
+        <>
+          <AccountBoxIcon sx={{ marginRight: 1 }} />
+          <Typography color="inherit" variant="h6" sx={{ marginRight: 2 }}>
+            {user.username}
+          </Typography>
+        </>
+      )}
+      {user ? (
+        <Button color="inherit" onClick={handleLogout}>
+          <LogoutIcon sx={{ marginLeft: 1 }} />
+          Logout
+        </Button>
+      ) : (
+        <Button color="inherit" component={Link} to="/login">
+          <LoginIcon sx={{ marginLeft: 1 }} />
+          Login
+        </Button>
+      )}
+    </Box>
+  );
+};
+
+export default AuthButtons;
