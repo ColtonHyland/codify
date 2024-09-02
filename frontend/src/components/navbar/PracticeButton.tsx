bb56b459016f608a925f85a8a7cddbb6e499cfee
Colton - Work <colthyland@gmail.com>
2024-09-02 14:58:26 -0700

    practice button

---
+++ frontend/src/components/navbar/PracticeButton.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import ImportContactsOutlinedIcon from "@mui/icons-material/ImportContactsOutlined";
+
+const PracticeButton: React.FC = () => (
+  <Button
+    color="inherit"
+    component={Link}
+    to="/questions"
+    sx={{
+      fontSize: "1rem",
+      padding: "6px 12px",
+      border: "2px solid white",
+      borderRadius: "12px",
+      "&:hover": {
+        backgroundColor: "rgba(255, 255, 255, 0.1)",
+      },
+    }}
+  >
+    <ImportContactsOutlinedIcon sx={{ marginRight: 1 }} />
+    Practice
+  </Button>
+);
+
+export default PracticeButton;
