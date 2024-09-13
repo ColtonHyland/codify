d5dbac4c77dc4d5da2763a87aa2f3f902ff00dcd
Colton - Work <colthyland@gmail.com>
2024-09-13 15:16:31 -0700

    circular loading for protected routes

--- frontend/src/utils/ProtectedRoute.tsx
+++ frontend/src/utils/ProtectedRoute.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import { CircularProgress, Grid } from '@mui/material';
 import { useAuth } from '../contexts/AuthContext';
 
 interface ProtectedRouteProps {
@@ -10,8 +11,11 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element }) => {
   const { user } = useAuth();
 
   if (user === undefined) {
-    // Return a loading indicator or nothing while determining the auth state
-    return <div>Loading...</div>;
+    return (
+      <Grid container justifyContent="center" alignItems="center" style={{ height: "100vh" }}>
+        <CircularProgress />
+      </Grid>
+    );
   }
 
   if (!user) {
